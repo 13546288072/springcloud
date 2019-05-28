7a54741556c31860947ef13611ecdc1bd93639ee
Administrator <Administrator@PC-20190426OJVW>
2019-05-28 10:59:18 +0800

    代码提交

--- microservicecloud-consumer-dept-80/src/main/java/com/ymt/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/ymt/springcloud/DeptConsumer80_App.java
@@ -2,8 +2,15 @@ package com.ymt.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+import com.ymt.myRule.MySelfRule;
 
 @SpringBootApplication
+@EnableEurekaClient     //本服务启动后会自动注册进eureka服务中
+// @RibbonClient  在启动该微服务的时候就能去加载我们的自定义Ribbon配置类，从而使配置生效，形如：
+@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration=MySelfRule.class)
 public class DeptConsumer80_App {
 
 	public static void main(String[] args) {
--- microservicecloud-consumer-dept-80/src/main/java/com/ymt/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/ymt/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.ymt.springcloud.cfgbeans;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ConfigBean {
 	@Bean
+	@LoadBalanced    //@LoadBalanced 获得Rest时加入Ribbon的配置
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
